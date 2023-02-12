88fb0498958a160e5991ac74c4ba7558996ce0f2
lshoo <lshoo36@gmail.com>
2023-02-12 23:56:33 +0800

    Add actionkv update, insert, delete, find fuctions

--- actionkv/src/lib.rs
+++ actionkv/src/lib.rs
@@ -1,8 +1,8 @@
 use std::fs::OpenOptions;
-use std::io::{self, BufReader, Read, Seek, SeekFrom};
+use std::io::{self, BufReader, BufWriter, Read, Seek, SeekFrom, Write};
 use std::{collections::HashMap, fs::File, path::Path};
 
-use byteorder::{LittleEndian, ReadBytesExt};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use crc::crc32;
 use serde_derive::{Deserialize, Serialize};
 
@@ -57,28 +57,58 @@ impl ActionKV {
         Ok(())
     }
 
-    pub fn get(&mut self, _key: &ByteStr) -> io::Result<Option<ByteString>> {
-        // let position = match self.index_at(key) {
-        //     None => return Ok(None),
-        //     Some(position) => *position,
-        // };
+    pub fn get(&mut self, key: &ByteStr) -> io::Result<Option<ByteString>> {
+        let position = match self.index.get(key) {
+            None => return Ok(None),
+            Some(position) => *position,
+        };
 
-        // let kv = self.get_at(position)?;
+        let kv = self.get_at(position)?;
 
-        // Ok(Some(kv.value))
-        todo!()
+        Ok(Some(kv.value))
     }
 
-    pub fn delete(&mut self, _key: &ByteStr) -> io::Result<()> {
-        todo!()
+    pub fn find(&mut self, target: &ByteStr) -> io::Result<Option<(u64, ByteString)>> {
+        let mut f = BufReader::new(&mut self.f);
+        let mut found: Option<(u64, ByteString)> = None;
+
+        loop {
+            let position = f.seek(SeekFrom::Current(0))?;
+            let maybe_kv = Self::process_record(&mut f);
+            let kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(err) => match err.kind() {
+                    io::ErrorKind::UnexpectedEof => {
+                        break;
+                    }
+                    _ => return Err(err),
+                },
+            };
+
+            if kv.key == target {
+                found = Some((position, kv.value));
+            }
+        }
+
+        Ok(found)
     }
 
-    pub fn insert(&mut self, _key: &ByteStr, value: &ByteStr) -> io::Result<()> {
-        todo!()
+    #[inline]
+    pub fn delete(&mut self, key: &ByteStr) -> io::Result<()> {
+        self.insert(key, b"")
     }
 
-    pub fn update(&mut self, _key: &ByteStr, value: &ByteStr) -> io::Result<()> {
-        todo!()
+    pub fn insert(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<()> {
+        let position = self.insert_but_ignore_index(key, value)?;
+
+        self.index.insert(key.to_vec(), position);
+
+        Ok(())
+    }
+
+    #[inline]
+    pub fn update(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<()> {
+        self.insert(key, value)
     }
 
     fn process_record<R: Read>(f: &mut R) -> io::Result<KeyValuePair> {
@@ -105,5 +135,44 @@ impl ActionKV {
         let key = data;
 
         Ok(KeyValuePair { key, value })
+    }
+
+    pub fn insert_but_ignore_index(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<u64> {
+        let mut f = BufWriter::new(&mut self.f);
+
+        let key_len = key.len();
+        let val_len = value.len();
+
+        let mut tmp = ByteString::with_capacity(key_len + val_len);
+
+        for byte in key {
+            tmp.push(*byte);
+        }
+
+        for byte in value {
+            tmp.push(*byte);
+        }
+
+        let checksum = crc32::checksum_ieee(&tmp);
+
+        let next_byte = SeekFrom::End(0);
+
+        let current_position = f.seek(SeekFrom::Current(0))?;
+        f.seek(next_byte)?;
+
+        f.write_u32::<LittleEndian>(checksum)?;
+        f.write_u32::<LittleEndian>(key_len as u32)?;
+        f.write_u32::<LittleEndian>(val_len as u32)?;
+        f.write_all(&tmp)?;
+
+        Ok(current_position)
+    }
+
+    pub fn get_at(&mut self, position: u64) -> io::Result<KeyValuePair> {
+        let mut f = BufReader::new(&mut self.f);
+        f.seek(SeekFrom::Start(position))?;
+        let kv = ActionKV::process_record(&mut f)?;
+
+        Ok(kv)
     }
 }
