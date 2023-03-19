64c26745d5a0a7b26b912fea8e5330e9010bd128
lshoo <lshoo36@gmail.com>
2023-03-19 22:07:36 +0800

    Add functional applicative functions

--- effective/examples/applicative_parsing_result.rs
+++ effective/examples/applicative_parsing_result.rs
@@ -1,35 +1,41 @@
-struct Magic<T>(Result<T, String>);
+pub struct Magic<T>(Result<T, String>);
 
 impl<T> Magic<T> {
-    fn run(self) -> Result<T, String> {
+    pub fn run(self) -> Result<T, String> {
         self.0
     }
 
-    fn flat_map<R, F: Fn(T) -> Result<R, String>>(self, f: F) -> Magic<R> {
+    pub fn flat_map<R, F: Fn(T) -> Result<R, String>>(self, f: F) -> Magic<R> {
         match self.run() {
             Ok(t) => Magic(f(t)),
             Err(e) => Magic(Err(e)),
         }
     }
-}
 
-impl<T> Magic<T> {
-    fn map<R, F: Fn(T) -> R>(self, f: F) -> Magic<R> {
+    pub fn map<R, F: Fn(T) -> R>(self, f: F) -> Magic<R> {
         self.flat_map(|t| Ok(f(t)))
     }
 
-    fn zip<R>(self, other: Magic<R>) -> Magic<(T, R)> {
+    pub fn zip<R>(self, other: Magic<R>) -> Magic<(T, R)> {
         match (self.0, other.0) {
             (Ok(t), Ok(r)) => Magic(Ok((t, r))),
             (_, Err(err)) | (Err(err), _) => Magic(Err(err)),
         }
     }
 
-    fn alt(self, other: Self) -> Self {
+    pub fn alt(self, other: Self) -> Self {
         match &self.0 {
             Ok(_) => self,
             Err(_) => other,
         }
+    }
+
+    pub fn pure(val: T) -> Self {
+        Self(Ok(val))
+    }
+
+    pub fn fail(msg: &str) -> Self {
+        Self(Err(msg.into()))
     }
 }
 
@@ -46,4 +52,10 @@ fn main() {
     let zip2 = Magic(Ok(30));
     let zip = zip1.zip(zip2);
     println!("{:?}", zip.0);
+
+    let nick = Magic::fail("No nick name give");
+    let fullname = Magic::pure("Bob the Maginficent");
+    let name = nick.alt(fullname);
+
+    println!("{:?}", name.0);
 }
