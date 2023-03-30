0895da7d5c496a45eee2db101a6c2804b1511c26
lshoo <lshoo36@gmail.com>
2023-03-30 18:54:24 +0800

    fix fmt

--- concurrencies/src/ractor_ping.rs
+++ concurrencies/src/ractor_ping.rs
@@ -1,23 +1,22 @@
-use ractor::{Actor, ActorProcessingErr, ActorRef, Message,};
+use ractor::{Actor, ActorProcessingErr, ActorRef, Message};
 
-
-// Define Actor 
+// Define Actor
 pub struct PingPong;
 
 // Define message(event)
 #[derive(Debug, Clone)]
 pub enum Event {
-    Add(u64), 
+    Add(u64),
     Sub(u64),
 }
 
 // #[cfg(feature = "culster")]
-impl Message for Event { }
+impl Message for Event {}
 
 impl Event {
     fn next(&self) -> Self {
         match self {
-            Self::Add(v) => Self::Sub( v / 2),
+            Self::Add(v) => Self::Sub(v / 2),
             Self::Sub(v) => Self::Add(*v),
         }
     }
@@ -30,29 +29,31 @@ impl Event {
     }
 }
 
-// Actor logic 
+// Actor logic
 #[async_trait::async_trait]
 impl Actor for PingPong {
     type Msg = Event;
     type State = u64;
     type Arguments = ();
 
-    async fn pre_start(&self, actor_ref: ActorRef<Self>, _: ()) -> Result<Self::State, ActorProcessingErr> {
+    async fn pre_start(
+        &self,
+        actor_ref: ActorRef<Self>,
+        _: (),
+    ) -> Result<Self::State, ActorProcessingErr> {
         actor_ref.send_message(Event::Add(20)).unwrap();
         Ok(0)
     }
 
     async fn handle(
-        &self, 
+        &self,
         actor_ref: ActorRef<Self>,
         msg: Self::Msg,
         state: &mut Self::State,
     ) -> Result<(), ActorProcessingErr> {
         if *state < 10 {
             msg.print();
-            actor_ref
-                .send_message(msg.next())
-                .unwrap();
+            actor_ref.send_message(msg.next()).unwrap();
             *state += 1;
         } else {
             actor_ref.stop(None);
@@ -62,10 +63,9 @@ impl Actor for PingPong {
     }
 }
 
-pub async fn run()  {
+pub async fn run() {
     let (_, actor_handle) = Actor::spawn(Some("PingPongActor".into()), PingPong, ())
         .await
         .expect("Failed to start actor");
     actor_handle.await.expect("Actor failed to exit cleanly");
 }
-
