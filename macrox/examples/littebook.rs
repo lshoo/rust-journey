c73f5ca0121aee8a742fe018039b4c957a5e8ec8
lshoo <lshoo36@gmail.com>
2024-01-10 16:07:35 +0800

    Add delcarative full example

--- macrox/examples/littebook.rs
+++ macrox/examples/littebook.rs
@@ -1,20 +1,106 @@
 macro_rules! vec_str {
     ($($e:expr), *) => {
         {
-            let mut v = Vec::new();
-            $(
-                v.push($e.to_string());
-            )*
+            // let mut v = Vec::new();
+            // $(
+            //     v.push($e.to_string());
+            // )*
 
-            v
+            // v
+            vec![$($e.to_string()), *]
         }
     };
  }
+
+macro_rules! count_exprs {
+    () => (0);
+    ($head: expr) => (1);
+    ($head: expr, $($tail: expr), *) => (1 + count_exprs!($($tail), *));
+ }
+
+macro_rules! recurrence {
+    ( $seq:ident [ $ind:ident ]: $sty:ty = $($inits:expr),+ ; ... ; $recur:expr ) => {
+
+        {
+            use std::ops::Index;
+
+            const MEM_SIZE: usize = count_exprs!($($inits),+);
+
+            struct Recurrence {
+                mem: [$sty; MEM_SIZE],
+                pos: usize,
+            }
+
+            struct IndexOffset<'a> {
+                slice: &'a [$sty; MEM_SIZE],
+                offset: usize,
+            }
+
+            impl<'a> Index<usize> for IndexOffset<'a> {
+                type Output = $sty;
+
+                #[inline(always)]
+                fn index(&self, index: usize) -> & $sty {
+                    use std::num::Wrapping;
+
+                    let index = Wrapping(index);
+                    let offset = Wrapping(self.offset);
+                    let window = Wrapping(MEM_SIZE);
+
+                    let real_index = index - offset + window;
+                    &self.slice[real_index.0]
+                }
+            }
+
+            impl Iterator for Recurrence {
+                type Item = $sty;
+
+                #[inline]
+                fn next(&mut self) -> Option<$sty> {
+                    if self.pos < MEM_SIZE {
+                        let next_val = self.mem[self.pos];
+                        self.pos += 1;
+                        Some(next_val)
+                    } else {
+                        let next_val = {
+                            let $ind = self.pos;
+
+                            let $seq = IndexOffset { slice: &self.mem, offset: $ind };
+                            $recur
+                        };
+
+                        {
+                            use std::mem::swap;
+
+                            let mut swap_tmp = next_val;
+                            for i in (0..MEM_SIZE).rev() {
+                                swap(&mut swap_tmp, &mut self.mem[i]);
+                            }
+                        }
+
+                        self.pos += 1;
+                        Some(next_val)
+                    }
+                }
+            }
+
+            Recurrence { mem: [$($inits),+], pos: 0 }
+        }
+    };
+}
 
 fn main() {
     let v = vec_str![1, "a", true, 3.14159f32];
 
     for i in v {
+        println!("{i}");
+    }
+
+    let len = count_exprs![1, "a", true];
+    println!("{len}");
+
+    let fib = recurrence![a[n]: u64 = 0, 1; ... ; a[n-1] + a[n-2]];
+    for i in fib.take(10) {
         println!("{i}");
     }
 }
